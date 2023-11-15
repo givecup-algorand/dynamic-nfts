63b66ad268d4135ea10df72aaf50b536b2db6134
lucasmanea21 <lucasmanea@Yahoo.com>
2023-11-15 19:19:07 +0200

    update

--- smart_contracts/dynamic_nfts/contract.py
+++ smart_contracts/dynamic_nfts/contract.py
@@ -1,11 +1,13 @@
 from beaker import Application, GlobalStateValue
-from pyteal import Seq, Txn, TxnType, TxnField, Int, Bytes, abi, TealType, Approve, Expr, InnerTxnBuilder, InnerTxn
+from pyteal import Seq, Txn, TxnType, TxnField, Int, Bytes, abi, TealType, Approve, Expr, InnerTxnBuilder, InnerTxn, Assert
 
 
 class GlobalState:
     # Define global state for asset id and owner
     asset_id = GlobalStateValue(stack_type=TealType.uint64, default=Int(0))
     owner = GlobalStateValue(stack_type=TealType.bytes, default=Bytes(""))
+    accessory_id = GlobalStateValue(stack_type=TealType.uint64, default=Int(0))
+    case_id = GlobalStateValue(stack_type=TealType.uint64, default=Int(0))
 
 
 app = Application("DynamicNFT", state=GlobalState())
@@ -41,10 +43,30 @@ def update_url(v: abi.String) -> Expr:
         InnerTxnBuilder.Begin(),
         InnerTxnBuilder.SetFields({
             TxnField.type_enum: TxnType.AssetConfig,
-            TxnField.config_asset_id: app.state.asset_id,
+            # TxnField.config_asset_id: app.state.asset_id,
             TxnField.config_asset_url: v.get(),
         }),
         InnerTxnBuilder.Submit(),
+        Approve()
+    )
+
+
+@app.external
+def integrate_accessory(accessory_id: abi.Uint64) -> Expr:
+    # Verify ownership and attach accessory to Cup NFT
+    return Seq(
+        Assert(Txn.sender() == app.state.owner),
+        app.state.accessory_id.set(accessory_id.get()),
+        Approve()
+    )
+
+
+@app.external
+def open_case(case_id: abi.Uint64) -> Expr:
+    # Logic to open a Case NFT
+    return Seq(
+        Assert(Txn.sender() == app.state.owner),
+        # logic for opening case and generating NFT
         Approve()
     )
 
