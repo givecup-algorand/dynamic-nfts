40cf3f5f80a7bd2962bf3182d26496f7ece0eec1
lucasmanea21 <lucasmanea@Yahoo.com>
2023-11-15 12:23:24 +0200

    added contracts

--- smart_contracts/dynamic_nfts/contract.py
+++ smart_contracts/dynamic_nfts/contract.py
@@ -1,10 +1,54 @@
-import beaker
-import pyteal as pt
+from beaker import Application, GlobalStateValue
+from pyteal import Seq, Txn, TxnType, TxnField, Int, Bytes, abi, TealType, Approve, Expr, InnerTxnBuilder, InnerTxn
 
 
-app = beaker.Application("dynamic_nfts")
+class GlobalState:
+    # Define global state for asset id and owner
+    asset_id = GlobalStateValue(stack_type=TealType.uint64, default=Int(0))
+    owner = GlobalStateValue(stack_type=TealType.bytes, default=Bytes(""))
+
+
+app = Application("DynamicNFT", state=GlobalState())
+
+
+@app.create
+def create():
+    return Seq(
+        # Create the NFT (ASA) with total supply of 1
+        InnerTxnBuilder.Begin(),
+        InnerTxnBuilder.SetFields({
+            TxnField.type_enum: TxnType.AssetConfig,
+            TxnField.config_asset_total: Int(1),
+            TxnField.config_asset_decimals: Int(0),
+            TxnField.config_asset_unit_name: Bytes("UNIQUE"),
+            TxnField.config_asset_name: Bytes("DynamicNFT"),
+            TxnField.config_asset_url: Bytes("https://initial.url"),
+        }),
+        InnerTxnBuilder.Submit(),
+
+        # Store the created asset id and owner in global state
+        app.state.asset_id.set(InnerTxn.created_asset_id()),
+        app.state.owner.set(Txn.sender()),
+
+        Approve()
+    )
 
 
 @app.external
-def hello(name: pt.abi.String, *, output: pt.abi.String) -> pt.Expr:
-    return output.set(pt.Concat(pt.Bytes("Hello, "), name.get()))
+def update_url(v: abi.String) -> Expr:
+    # Only the owner can update the URL
+    return Seq(
+        InnerTxnBuilder.Begin(),
+        InnerTxnBuilder.SetFields({
+            TxnField.type_enum: TxnType.AssetConfig,
+            TxnField.config_asset_id: app.state.asset_id,
+            TxnField.config_asset_url: v.get(),
+        }),
+        InnerTxnBuilder.Submit(),
+        Approve()
+    )
+
+
+@app.external(read_only=True)
+def get_asset_id(*, output: abi.Uint64) -> Expr:
+    return output.set(app.state.asset_id)
--- smart_contracts/dynamic_nfts/deploy_config.py
+++ smart_contracts/dynamic_nfts/deploy_config.py
@@ -1,36 +1,54 @@
 import logging
-
-import algokit_utils
 from algosdk.v2client.algod import AlgodClient
 from algosdk.v2client.indexer import IndexerClient
+import algokit_utils
+from contract import DynamicNFT  # Import the Dynamic NFT contract
 
 logger = logging.getLogger(__name__)
 
 
-# define deployment behaviour based on supplied app spec
 def deploy(
     algod_client: AlgodClient,
     indexer_client: IndexerClient,
     app_spec: algokit_utils.ApplicationSpecification,
     deployer: algokit_utils.Account,
 ) -> None:
-    from smart_contracts.artifacts.dynamic_nfts.client import (
-        DynamicNftsClient,
-    )
-
-    app_client = DynamicNftsClient(
+    # Create an instance of the DynamicNFT client
+    app_client = algokit_utils.ApplicationClient(
         algod_client,
-        creator=deployer,
+        DynamicNFT(),  # Initialize your Dynamic NFT contract
+        creator=deployer.address,
+        signer=deployer.signer,  # Assuming your Account class has a signer method
         indexer_client=indexer_client,
     )
+
+    # Deploy the Dynamic NFT application
     app_client.deploy(
         on_schema_break=algokit_utils.OnSchemaBreak.AppendApp,
         on_update=algokit_utils.OnUpdate.AppendApp,
     )
 
-    name = "world"
-    response = app_client.hello(name=name)
+    # Log deployment details
     logger.info(
-        f"Called hello on {app_spec.contract.name} ({app_client.app_id}) "
-        f"with name={name}, received: {response.return_value}"
+        f"Deployed Dynamic NFT Contract {app_spec.contract.name} "
+        f"with App ID: {app_client.app_id}"
     )
+
+
+# Example usage
+if __name__ == "__main__":
+    # Setup Algod and Indexer clients
+    algod_client = AlgodClient("api-token", "http://localhost:4001")
+    indexer_client = IndexerClient("indexer-token", "http://localhost:8980")
+
+    # Define the deployer account (adjust with actual account details)
+    deployer = algokit_utils.Account.from_mnemonic(
+        "your 25-word mnemonic here")
+
+    # Define application specification (adjust as per your setup)
+    app_spec = algokit_utils.ApplicationSpecification(
+        contract=algokit_utils.Contract(name="DynamicNFT")
+    )
+
+    # Deploy the application
+    deploy(algod_client, indexer_client, app_spec, deployer)
